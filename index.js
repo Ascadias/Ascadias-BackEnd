3de1aaee8b0406e5a08bd3ee3bc00064d80bc090
Marcelo Fernandez <goushart@gmail.com>
2021-06-06 02:30:06 -0300

    Agrego archivos .json

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+const port = process.env.PORT || 8080;
+
+const about = require('./JSON/About.json');
+const portfolio = require('./JSON/Portfolio.json');
+
+app.get('/about', (req, res) => {
+    res.json(about);
+});
+
+app.get('/portfolio', (req, res) => {
+    res.json(portfolio);
+});
+
+app.get('/', (req, res) => {
+    
+});
+
+app.listen(port, () =>{
+    console.log(`listening on port : ${port}`);
+});
